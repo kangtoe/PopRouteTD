4d9a3021690e3be7b96d71196255a77c941bc0af
kangtoe <kangtoe99@gmail.com>
2026-02-19 09:12:26 +0900

    refactor: 적 사망 시 아이템 드롭 시스템 제거

--- Assets/Scripts/Item/ItemSpawner.cs
+++ Assets/Scripts/Item/ItemSpawner.cs
@@ -4,10 +4,6 @@ public class ItemSpawner : MonoBehaviour
 {
     public static ItemSpawner Instance { get; private set; }
 
-    [Header("드롭 설정")]
-    [SerializeField, Range(0f, 1f)] private float dropChance = 0.1f;
-    [SerializeField] private int dropGold = 5;
-
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -16,24 +12,6 @@ public class ItemSpawner : MonoBehaviour
             return;
         }
         Instance = this;
-    }
-
-    private void OnEnable()
-    {
-        Enemy.OnEnemyDestroyed += OnEnemyDestroyed;
-    }
-
-    private void OnDisable()
-    {
-        Enemy.OnEnemyDestroyed -= OnEnemyDestroyed;
-    }
-
-    private void OnEnemyDestroyed(Enemy enemy)
-    {
-        if (enemy.CurrentLayer != EnemyLayer.Red) return;
-        if (Random.value > dropChance) return;
-
-        SpawnItem(enemy.transform.position, dropGold);
     }
 
     public void SpawnItem(Vector3 position, int gold)
